beab136f3676c6b97018eb099d7a8d7d07dff6ef
Anwesh43 <anweshthecool0@gmail.com>
2018-07-25 00:58:06 +0530

    adding LinkedLalView class

--- linkedlalview/src/main/java/com/anwesh/uiprojects/linkedlalview/LinkedLALView.kt
+++ linkedlalview/src/main/java/com/anwesh/uiprojects/linkedlalview/LinkedLALView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.linkedlalview
 /**
  * Created by anweshmishra on 25/07/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+
+class LinkedLALView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
