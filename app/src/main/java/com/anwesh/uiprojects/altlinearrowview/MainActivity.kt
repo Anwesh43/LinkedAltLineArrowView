2905a8648129a2f8f9e4205e57085da618cc1052
Anwesh43 <anweshthecool0@gmail.com>
2018-07-25 02:04:12 +0530

    using LinkedLALView in Activity

--- app/src/main/java/com/anwesh/uiprojects/altlinearrowview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/altlinearrowview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.altlinearrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedlalview.LinkedLALView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedLALView.create(this)
     }
 }
