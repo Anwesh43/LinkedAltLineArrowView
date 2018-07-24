f80a94c4e3ba329ff922435de843530343105bd5
Anwesh43 <anweshthecool0@gmail.com>
2018-07-25 01:19:38 +0530

    adding extension methods

--- linkedlalview/src/main/java/com/anwesh/uiprojects/linkedlalview/LinkedLALView.kt
+++ linkedlalview/src/main/java/com/anwesh/uiprojects/linkedlalview/LinkedLALView.kt
@@ -10,6 +10,41 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 
+val NODES : Int = 5
+
+fun Canvas.drawLALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = Math.min(w, h) / (2 * NODES)
+    val index : Int = i % 2
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
+    val fc : Float = (1 - sc1) * index + sc1 * (1 - index)
+    save()
+    translate(i * gap + gap/2, 0f)
+    save()
+    translate(-gap/4 + gap * sc2, 0f)
+    for (j in 0..1) {
+        save()
+        rotate(30 * (1 - 2 * j) * fc)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawInAxis(cb : () -> Unit) {
+    save()
+    translate(width.toFloat() / 2, height.toFloat() / 2)
+    for (i in 0..4) {
+        save()
+        rotate(90f * i)
+        cb()
+        restore()
+    }
+    restore()
+}
+
 class LinkedLALView (ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
